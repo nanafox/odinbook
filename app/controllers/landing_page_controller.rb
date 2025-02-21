0ca1514df1863773ebbbab13f3cb0dc7376e0a31
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-02-21 00:37:56 +0000

    feat: Temporarily redirect users to their after login

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,6 +2,14 @@
 class LandingPageController < ApplicationController
   skip_before_action :ensure_profile_complete
 
+  before_action :redirect_to_home, if: :current_user
+
   def index
   end
+
+  private
+
+    def redirect_to_home
+      redirect_to current_user_profile_path
+    end
 end
