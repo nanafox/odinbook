8e43174fdf830190077d654f8e92437f4dbf0c01
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-02-21 00:43:43 +0000

    feat: Add `owner?` to base policy and update profile policy to use it

--- app/policies/profile_policy.rb
+++ app/policies/profile_policy.rb
@@ -1,9 +1,18 @@
+# Policy for the profile actions
 class ProfilePolicy < ApplicationPolicy
-  def read?
+  def new?
+    user.profile.nil?
+  end
+
+  def show?
     true
   end
 
+  def edit?
+    owner?
+  end
+
   def update?
-    user.id == record.user_id
+    owner?
   end
 end
