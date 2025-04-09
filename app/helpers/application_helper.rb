1131b574ffaf001c887e2e9b3b66bf5630d53720
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-04-09 00:14:10 +0000

    feat: Use a helper method to avoid complex operation in the view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def border_separator_color
     "border-slate-700"
   end
+
+  # Builds a profile image with the user's first and last names
+  def fallback_avatar_for(user)
+    user.name.split(" ").map { |n| n[0] }.join
+  end
 end
